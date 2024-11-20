4bf3d83294ad19ffebf9d6802dcad21d46c475da
ankitkulhade <ankitkulhade123@gmail.com>
2024-11-21 02:09:04 +0530

    First Commit

---
+++ src/main/java/com/LearningSpring/LearningSpring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.LearningSpring.LearningSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
